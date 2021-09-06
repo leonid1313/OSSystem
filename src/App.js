956099679fe3ed3048c56010ecca18e3e38b1050
Leonid <manu31051996@gmail.com>
2021-09-06 10:19:22 +0300

    add demo link

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css'
 
+import Catalog from './pages/Catalog/Catalog.jsx'
 import Header from './components/Header/Header.jsx'
-import Search from './pages/Search/Search.jsx'
 import About from './pages/About/About.jsx'
 import Loader from './components/Loader/Loader.jsx'
 import Footer from './components/Footer/Footer.jsx';
@@ -29,7 +29,7 @@ function App () {
           <>
             <Router basename="/OSSystem">
               <Header />
-              <Route path='/' exact component={Search} />
+              <Route path='/' exact component={Catalog} />
               <Route path="/about/:item" exact component={About}/>
               <Footer/>
             </Router>
